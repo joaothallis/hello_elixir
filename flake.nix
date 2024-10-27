ec44b6d76a49a285c92808a99cd0233c4cc17f00
João Thallis <joaothallis@icloud.com>
2024-10-27 15:00:56 +0100

    add module

--- flake.nix
+++ flake.nix
@@ -26,8 +26,7 @@
           let
             version = "0.1.0";
             src = ./.;
-            mixNixDeps = with pkgs; with beamPackages;import ./mix_deps.nix { inherit lib beamPackages; };
-            # mixNixDeps = import ./deps.nix { inherit lib beamPackage; };
+            mixNixDeps = with pkgs; with beamPackages; import ./mix_deps.nix { inherit lib beamPackages; };
             translatedPlatform =
               {
                 aarch64-darwin = "macos-arm64";
@@ -39,7 +38,6 @@
               .${system};
           in
           rec {
-            # inner expression
             default = erlangPackages.mixRelease {
               inherit version src mixNixDeps;
               pname = "hello";
@@ -48,6 +46,64 @@
                 ${elixir}/bin/mix release --no-deps-check
               '';
             };
+            nixosModule =
+              {
+                config,
+                lib,
+                pkgs,
+                ...
+              }:
+              let
+                cfg = config.services.hello;
+                user = "hello";
+                dataDir = "/var/lib/hello";
+              in
+              {
+                options.services.hello = {
+                  enable = lib.mkEnableOption "hello";
+                };
+                config = lib.mkIf cfg.enable {
+                  users.users.${user} = {
+                    isSystemUser = true;
+                    group = user;
+                    home = dataDir;
+                    createHome = true;
+                  };
+                  users.groups.${user} = { };
+
+                  systemd.services = {
+                    hello = {
+                      description = "Start up the homepage";
+                      wantedBy = [ "multi-user.target" ];
+                      script = ''
+                        # Elixir does not start up if `RELEASE_COOKIE` is not set,
+                        # even though we set `RELEASE_DISTRIBUTION=none` so the cookie should be unused.
+                        # Thus, make a random one, which should then be ignored.
+                        export RELEASE_COOKIE=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 20)
+
+                        ${default}/bin/hello eval Hello.hello
+                      '';
+                      serviceConfig = {
+                        User = user;
+                        WorkingDirectory = "${dataDir}";
+                        Group = user;
+                      };
+
+                      environment = {
+                        # Disable Erlang's distributed features
+                        RELEASE_DISTRIBUTION = "none";
+                        # Additional safeguard, in case `RELEASE_DISTRIBUTION=none` ever
+                        # stops disabling the start of EPMD.
+                        ERL_EPMD_ADDRESS = "127.0.0.1";
+                        # Home is needed to connect to the node with iex
+                        HOME = "${dataDir}";
+                        # PORT = toString cfg.port;
+                      };
+                    };
+                  };
+                };
+              };
+
           };
       }
     );
